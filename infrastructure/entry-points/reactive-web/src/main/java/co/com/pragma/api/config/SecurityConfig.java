1005b98e99c80ea22330592da941bc2d2185e016
Roberto Londoño <roberthdrums@gmail.com>
2025-09-14 00:08:56 -0500

    fix actuator and jwt

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/config/SecurityConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import co.com.pragma.api.constants.ReportsWebKeys;
 import co.com.pragma.api.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -23,7 +25,20 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityWebFilterChain filterChain(ServerHttpSecurity http, JwtFilter jwtFilter) {
+  @Order(1)
+  public SecurityWebFilterChain actuatorFilterChain(ServerHttpSecurity http) {
+    return http
+      .securityMatcher(new PathPatternParserServerWebExchangeMatcher(ReportsWebKeys.ALLOWED_PATH_ACTUATOR))
+      .csrf(ServerHttpSecurity.CsrfSpec::disable)
+      .authorizeExchange(exchangeSpec -> exchangeSpec
+        .anyExchange()
+        .permitAll())
+      .build();
+  }
+
+  @Bean
+  @Order(2)
+  public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http, JwtFilter jwtFilter) {
     return http
       .csrf(ServerHttpSecurity.CsrfSpec::disable)
       .authorizeExchange(exchangeSpec -> exchangeSpec
@@ -39,5 +54,5 @@ public class SecurityConfig {
       .logout(ServerHttpSecurity.LogoutSpec::disable)
       .build();
   }
-}
 
+}
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/jwt/JwtFilter.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/jwt/JwtFilter.java
@@ -3,6 +3,8 @@ package co.com.pragma.api.jwt;
 import co.com.pragma.api.constants.ReportsWebKeys;
 import co.com.pragma.api.exception.AuthenticationApiException;
 import co.com.pragma.model.report.exception.ErrorEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -15,14 +17,18 @@ import reactor.util.annotation.NonNull;
 @Component
 public class JwtFilter implements WebFilter {
 
+  private static final Logger LOG = LoggerFactory.getLogger(JwtFilter.class);
+
   @Override
   @NonNull
   public Mono<Void> filter(ServerWebExchange exchange, @NonNull WebFilterChain chain) {
     ServerHttpRequest request = exchange.getRequest();
     String path = request.getPath().value();
+    LOG.info("Path filter jwt => {}", path);
     if (path.contains(ReportsWebKeys.STRING_AUTH) || path.contains(ReportsWebKeys.STRING_SWAGGER) || path.contains(ReportsWebKeys.STRING_DOCS) ||
-        path.contains(ReportsWebKeys.STRING_WEBJARS) || path.contains(ReportsWebKeys.STRING_ACTUATOR))
+        path.contains(ReportsWebKeys.STRING_WEBJARS) || path.contains(ReportsWebKeys.STRING_ACTUATOR)) {
       return chain.filter(exchange);
+    }
     String auth = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
     if(auth == null)
       return Mono.error(new AuthenticationApiException(ErrorEnum.INVALID_TOKEN, ReportsWebKeys.NO_TOKEN));
